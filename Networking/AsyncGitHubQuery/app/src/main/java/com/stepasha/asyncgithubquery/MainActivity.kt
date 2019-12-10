8c9c03afa9fc119d255aab948aca39608a87877e
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-12-10 13:35:28 -0700

    binding views
    including a searchbar

--- Networking/AsyncGitHubQuery/app/src/main/java/com/stepasha/asyncgithubquery/MainActivity.kt
+++ Networking/AsyncGitHubQuery/app/src/main/java/com/stepasha/asyncgithubquery/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.stepasha.asyncgithubquery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    private fun makeGithubSearchQuery() {
+        val githubQuery: String = mSearchBoxEditText.getText().toString()
+        val githubSearchUrl: URL? = NetworkUtils.buildUrl(githubQuery)
+        mUrlDisplayTextView.setText(githubSearchUrl.toString())
+        var githubSearchResults: String? = null
+        try {
+            githubSearchResults = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl)
+            mSearchResultsTextView.setText(githubSearchResults)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+       
     }
 }
