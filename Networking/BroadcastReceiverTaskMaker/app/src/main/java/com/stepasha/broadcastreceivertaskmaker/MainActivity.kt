5514d3051384e94feb853d9551a698aba386f868
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-12-12 16:14:04 -0700

    Broadcast receiver to listen to time

--- Networking/BroadcastReceiverTaskMaker/app/src/main/java/com/stepasha/broadcastreceivertaskmaker/MainActivity.kt
+++ Networking/BroadcastReceiverTaskMaker/app/src/main/java/com/stepasha/broadcastreceivertaskmaker/MainActivity.kt
@@ -2,13 +2,20 @@ package com.stepasha.broadcastreceivertaskmaker
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.IllegalArgumentException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +23,12 @@ class MainActivity : AppCompatActivity() {
     private val taskList = mutableListOf<String>()
     // bind it to the adapter
     private val adapter by lazy { makeAdapter(taskList) }
-
+    private val tickReceiver by lazy { makeBroadcastReceiver() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dateTimeTextView.text = getCurrentTimeStamp()
+        registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
 
         val intent = Intent(this, TaskDetailActivity::class.java)
         startActivityForResult(intent, Companion.ADD_TASK_REQUEST)
@@ -60,5 +69,36 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val ADD_TASK_REQUEST = 1
+        private const val LOG_TAG ="MainActivityLog"
+    private fun getCurrentTimeStamp():String {
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US)
+        val now = Date()
+        return simpleDateFormat.format(now)
+    }
+    }
+    private fun makeBroadcastReceiver():BroadcastReceiver{
+        return object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_TIME_TICK){
+                    dateTimeTextView.text = getCurrentTimeStamp()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dateTimeTextView.text = getCurrentTimeStamp()
+        registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            unregisterReceiver(tickReceiver)
+        }catch (e: IllegalArgumentException){
+            Log.e(MainActivity.LOG_TAG, "receiver not registered", e)
+        }
+
     }
 }
