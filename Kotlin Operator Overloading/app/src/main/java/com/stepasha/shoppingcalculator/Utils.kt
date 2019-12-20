d17d8007e5d8a2694fefe83768af823126387980
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-12-19 19:05:25 -0700

    polish

--- Kotlin Operator Overloading/app/src/main/java/com/stepasha/shoppingcalculator/Utils.kt
+++ Kotlin Operator Overloading/app/src/main/java/com/stepasha/shoppingcalculator/Utils.kt
@@ -3,18 +3,12 @@ package com.stepasha.shoppingcalculator
 import android.view.View
 
 
-operator fun View.plus(items: ShoppingItem):String{
+operator fun View.plus(sum: Double):Double{
 
-    return items.toString()
-
-}
-
-operator fun View.minus(items: MutableList<ShoppingItem>):String{
-
-
-    return items.toString()
+    return sum
 
 }
+
 
 operator fun View.times(sum: Double):Double{
 
