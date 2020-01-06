b7864da5057c160850c1e04025f21c69eaa33623
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-01-06 13:17:54 -0700

    detaching the project

--- Front End For Maps/bl/app/src/main/java/com/stepasha/buildinglocator/model/LoginResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.stepasha.buildinglocator.model
-
-data class LoginResponse(val error: Boolean, val message: String, val userId: Int, val token: String)
--- Front End For Maps/bl/app/src/main/java/com/stepasha/buildinglocator/model/RegisterResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.stepasha.buildinglocator.model
-
-data class RegisterResponse (val error: Boolean, val message: String, val token: String, val userId: Int)
